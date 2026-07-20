319f38f06528e59ee822f6ae8d4c6e743a958ef6
aki-riko <aki-riko@9li.life>
2026-07-20 22:43:59 +0800

    chore: prepare v1.0.17 release

--- tests/test_app_updater.py
+++ tests/test_app_updater.py
@@ -132,8 +132,8 @@ class AppUpdaterTests(unittest.TestCase):
         self.engine = self.controller._updater
 
     def test_real_configuration_builds_verified_engine_contract(self):
-        self.assertEqual(self.controller.version, "1.0.16")
-        self.assertEqual(self.controller.currentVersion, "v1.0.16")
+        self.assertEqual(self.controller.version, "1.0.17")
+        self.assertEqual(self.controller.currentVersion, "v1.0.17")
         self.assertEqual(self.controller.prismqmlVersion, "0.3.1.4")
         self.assertEqual(self.engine.repo, "aki-riko/ConfigPilot")
         self.assertEqual(self.engine.asset_keyword, "Setup")
@@ -146,8 +146,8 @@ class AppUpdaterTests(unittest.TestCase):
 
         self.controller.checkAutomatically()
         self.assertTrue(self.controller.checking)
-        self.engine.upToDate.emit("v1.0.16")
-        self.assertEqual(up_to_date, [("v1.0.16", False)])
+        self.engine.upToDate.emit("v1.0.17")
+        self.assertEqual(up_to_date, [("v1.0.17", False)])
 
         self.controller.checkManually()
         self.engine.checkFailed.emit("network down")
@@ -161,22 +161,22 @@ class AppUpdaterTests(unittest.TestCase):
         self.controller.checkAutomatically()
         self.controller.checkManually()
         self.assertEqual(self.engine.check_count, 1)
-        self.engine.upToDate.emit("v1.0.16")
-        self.assertEqual(results, [("v1.0.16", True)])
+        self.engine.upToDate.emit("v1.0.17")
+        self.assertEqual(results, [("v1.0.17", True)])
 
     def test_release_page_shell_open_runs_off_main_thread(self):
         main_thread = threading.get_ident()
 
         self.assertTrue(
             self.controller.openReleasePage(
-                "https://github.com/aki-riko/ConfigPilot/releases/tag/v1.0.16"
+                "https://github.com/aki-riko/ConfigPilot/releases/tag/v1.0.17"
             )
         )
         wait_until(lambda: bool(self.open_calls))
 
         self.assertEqual(
             self.open_calls[0][0],
-            "https://github.com/aki-riko/ConfigPilot/releases/tag/v1.0.16",
+            "https://github.com/aki-riko/ConfigPilot/releases/tag/v1.0.17",
         )
         self.assertNotEqual(self.open_calls[0][1], main_thread)
         self.assertFalse(self.controller.openReleasePage("file:///C:/Windows/System32"))
